466d555c06e47f5163b977742ffb6dcd4117b132
Luchohak <a00958934@gmail.com>
2023-12-07 17:34:50 -0600

    Setup User and Product relationship with Sequelize and create an initial user upon app start

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const expressHbs = require("express-handlebars");
 
+const Product = require("./models/product");
+const User = require("./models/user");
+
 const app = express();
 
 app.engine(
@@ -21,18 +24,42 @@ const sequelize = require("./util/database");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      //This will not store a JS Object, instead it is a Sequelize Object that will contain all its utility methods  
+      req.user =  user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+//would be the same as...
+User.hasMany(Product);
+
 sequelize
+  //.sync({ force: true })
   .sync()
   .then((result) => {
+    return User.findByPk(1);
     //console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Lucho", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    //console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
     console.log(err);
   });
-
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -8,6 +8,10 @@ var bodyParser = require("body-parser");
 
 var expressHbs = require("express-handlebars");
 
+var Product = require("./models/product");
+
+var User = require("./models/user");
+
 var app = express();
 app.engine("handlebars", expressHbs({
   layoutsDir: "views/layouts",
@@ -28,11 +32,38 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(express["static"](path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  User.findByPk(1).then(function (user) {
+    //This will not store a JS Object, instead it is a Sequelize Object that will contain all its utility methods  
+    req.user = user;
+    next();
+  })["catch"](function (err) {
+    return console.log(err);
+  });
+});
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
-sequelize.sync().then(function (result) {
-  //console.log(result);
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: "CASCADE"
+}); //would be the same as...
+
+User.hasMany(Product);
+sequelize //.sync({ force: true })
+.sync().then(function (result) {
+  return User.findByPk(1); //console.log(result);
+}).then(function (user) {
+  if (!user) {
+    return User.create({
+      name: "Lucho",
+      email: "test@test.com"
+    });
+  }
+
+  return user;
+}).then(function (user) {
+  //console.log(user);
   app.listen(3000);
 })["catch"](function (err) {
   console.log(err);
